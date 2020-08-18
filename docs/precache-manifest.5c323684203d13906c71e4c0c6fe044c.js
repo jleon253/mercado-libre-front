060685da44a0ad330f850c82d8caabc6cd46a6ee
jleon253 <jleon253@gmail.com>
2020-08-18 01:50:50 -0500

    Agregando carpeta docs para GitHub Pages

---
+++ docs/precache-manifest.5c323684203d13906c71e4c0c6fe044c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a0af5cac18c2e9b17727c3a79426e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d506ae17a25cfe80dc0",
+    "url": "./static/css/2.bc96f549.chunk.css"
+  },
+  {
+    "revision": "b7877d2470282496786b",
+    "url": "./static/css/main.1fca467a.chunk.css"
+  },
+  {
+    "revision": "8d506ae17a25cfe80dc0",
+    "url": "./static/js/2.e2ef8a6b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.e2ef8a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7877d2470282496786b",
+    "url": "./static/js/main.d2a9fd7f.chunk.js"
+  },
+  {
+    "revision": "88735dca8bf62e1c1ae0",
+    "url": "./static/js/runtime-main.6ffeff12.js"
+  },
+  {
+    "revision": "f63dc0b44d62fbbb031b8e88d9ff8c0e",
+    "url": "./static/media/mercado-libre-logo.f63dc0b4.svg"
+  },
+  {
+    "revision": "a7f473ae20d46a39d05060957cfc8a4d",
+    "url": "./static/media/search.a7f473ae.svg"
+  },
+  {
+    "revision": "fcda875ed78792912f1e43b3e90be70f",
+    "url": "./static/media/shipping.fcda875e.svg"
+  }
+]);
